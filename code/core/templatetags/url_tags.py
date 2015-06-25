33e7d06a20e934978e8664f1312f6431ad55369a
Stephanie Sydorko <stephanephanon@gmail.com>
2015-06-24 22:38:55 -0400

    continued changes to f/e per rohit and david.

--- code/core/templatetags/url_tags.py
+++ code/core/templatetags/url_tags.py
@@ -4,3 +4,7 @@ register = template.Library()
 @register.filter
 def replace_spaces(value):
     return value.replace(' ', '+')
+
+@register.filter
+def replace_underscores(value):
+    return value.replace('_', ' ')
