85dc23fc57a9027f51dea38bf857fcac8241dd97
Stephanie Sydorko <stephanephanon@gmail.com>
2015-07-03 21:24:52 -0400

    updating charts, added redirect to enforcement chart, added pie chart to adverse events chart

--- code/core/models.py
+++ code/core/models.py
@@ -6,6 +6,6 @@ class ContactInformation(models.Model):
     Model corresponding to Contact Us form fields
     """
     timestamp = models.DateTimeField(auto_now_add=True)
-    contact_name = models.CharField(max_length=100, null=True, blank=True)
+    contact_name = models.CharField(max_length=100, null=False, blank=False)
     contact_email = models.EmailField(max_length=200, null=False, blank=False)
     contact_comment = models.TextField(null=False, blank=False)
