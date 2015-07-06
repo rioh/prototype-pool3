0ac256acc5c694bae84e3036e3fdef90b1cb4b3d
Risa Ohara <risaohara@gmail.com>
2015-07-06 10:40:39 -0400

    moved static directory path

--- code/opendata_fda/settings.py
+++ code/opendata_fda/settings.py
@@ -87,11 +87,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, '.static')
+STATIC_ROOT = os.path.join(BASE_DIR, '.static3')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static3/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
